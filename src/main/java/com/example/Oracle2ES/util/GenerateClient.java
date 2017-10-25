1486fa89d42159f6fa6779f4f3b44de9e1ef9973
zhaojie <zhaojie@bonc.com.cn>
2017-10-25 10:34:09 +0800

    虚拟机上的入数程序

--- src/main/java/com/example/Oracle2ES/util/GenerateClient.java
+++ src/main/java/com/example/Oracle2ES/util/GenerateClient.java
@@ -3,7 +3,7 @@ package com.example.Oracle2ES.util;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -19,18 +19,17 @@ import java.net.UnknownHostException;
  */
 public class GenerateClient {
     private static Settings settings = Settings.builder()
-            .put("cluster.name", "dw3.0")//设置ES实例的名称
+            .put("cluster.name", "Logs_Collect_V1")//设置ES实例的名称
             .put("client.transport.sniff", true)//自动嗅探整个集群的状态，把集群中其他ES节点的ip添加到本地的客户端列表中
-            .put("xpack.security.user","yz_dw3_pro:INS8_evdj_55BV")
             .build();
 
     public static TransportClient getClient()throws UnknownHostException {
-        TransportClient client = new PreBuiltXPackTransportClient(settings)
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.161.24.82"), 9300))
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.161.24.83"), 9300))
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.161.24.84"), 9300));
+        TransportClient client = new PreBuiltTransportClient(settings)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.249.216.108"), 9300))
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.249.216.109"), 9300))
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.249.216.110"), 9300))
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.249.216.111"), 9300));
 //                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.31.2"), 9300));
         return client;
     }
-
 }
